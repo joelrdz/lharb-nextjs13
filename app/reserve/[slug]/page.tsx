6818373d351c97a91aea0f72e9b80d0ca383d59f
Joel Rdz <joelrdzh@gmail.com>
2023-02-09 18:47:50 -0600

    reserve page Header component

--- app/reserve/[slug]/page.tsx
+++ app/reserve/[slug]/page.tsx
@@ -1,4 +1,5 @@
 import NavBar from '../../components/NavBar';
+import Header from './components/Header';
 
 export default function Reserve() {
   return (
@@ -7,22 +8,7 @@ export default function Reserve() {
         <NavBar />
         <div className="border-t h-screen">
           <div className="py-9 w-3/5 m-auto">
-            {/* HEADER */}
-            <div>
-              <h3 className="font-bold">You're almost done!</h3>
-              <div className="mt-5 flex">
-                <img src="https://images.otstatic.com/prod1/49153814/2/medium.jpg" alt="" className="w-32 h-18 rounded" />
-                <div className="ml-4">
-                  <h1 className="text-3xl font-bold">Aiāna Restaurant Collective</h1>
-                  <div className="flex mt-3">
-                    <p className="mr-6">Tues, 22, 2023</p>
-                    <p className="mr-6">7:30 PM</p>
-                    <p className="mr-6">3 people</p>
-                  </div>
-                </div>
-              </div>
-            </div>
-            {/* HEADER */}
+            <Header />
             {/* FORM */}
             <div className="mt-10 flex flex-wrap justify-between w-[660px]">
               <input type="text" className="border rounded p-3 w-80 mb-4" placeholder="First name" />
