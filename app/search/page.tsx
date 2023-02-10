16e95a6213a0e36fcf3198a6b8205ac194f6649f
Joel Rdz <joelrdzh@gmail.com>
2023-02-09 18:14:28 -0600

    search page SearchSideBar component

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,5 +1,6 @@
 import NavBar from '../components/NavBar';
 import Header from './components/Header';
+import SearchSideBar from './components/SearchSideBar';
 
 export default function Search() {
   return (
@@ -8,33 +9,7 @@ export default function Search() {
         <NavBar />
         <Header />
         <div className="flex py-4 m-auto w-2/3 justify-between items-start">
-          {/* SEARCH SIDE BAR */}
-          <div className="w-1/5">
-            <div className="border-b pb-4">
-              <h1 className="mb-2">Region</h1>
-              <p className="font-light text-reg">Toronto</p>
-              <p className="font-light text-reg">Ottawa</p>
-              <p className="font-light text-reg">Montreal</p>
-              <p className="font-light text-reg">Hamilton</p>
-              <p className="font-light text-reg">Kingston</p>
-              <p className="font-light text-reg">Niagara</p>
-            </div>
-            <div className="border-b pb-4 mt-3">
-              <h1 className="mb-2">Cuisine</h1>
-              <p className="font-light text-reg">Mexican</p>
-              <p className="font-light text-reg">Italian</p>
-              <p className="font-light text-reg">Chinese</p>
-            </div>
-            <div className="mt-3 pb-4">
-              <h1 className="mb-2">Price</h1>
-              <div className="flex">
-                <button className="w-full text-reg font-light p-2 border rounded-l">$</button>
-                <button className="w-full text-reg font-light p-2 border-y border-r">$$</button>
-                <button className="w-full text-reg font-light p-2 border-y border-r rounded-r">$$$</button>
-              </div>
-            </div>
-          </div>
-          {/* SEARCH SIDE BAR */}
+          <SearchSideBar />
           <div className="w-5/6">
             {/* RESTAURANT CARD */}
             <div className="border-b flex pb-5">
