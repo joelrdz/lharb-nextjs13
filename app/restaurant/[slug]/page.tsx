3521accb7292034b897ff4b42bd9e2a8de4158f3
Joel Rdz <joelrdzh@gmail.com>
2023-02-09 18:25:20 -0600

    restaurant page ReservationCard component
    - and some cleaning

--- app/restaurant/[slug]/page.tsx
+++ app/restaurant/[slug]/page.tsx
@@ -3,6 +3,7 @@ import Description from './components/Description';
 import Header from './components/Header';
 import Images from './components/Images';
 import Rating from './components/Rating';
+import ReservationCard from './components/ReservationCard';
 import RestaurantNavBar from './components/RestaurantNavBar';
 import Reviews from './components/Reviews';
 import Title from './components/Title';
@@ -13,7 +14,6 @@ export default function RestaurantDetails() {
       <main className="max-w-screen-2xl m-auto bg-white">
         <NavBar />
         <Header />
-        {/* INFO SECTION */}
         <div className="flex m-auto w-2/3 justify-between items-start -mt-11">
           <div className="bg-white w-[70%] rounded p-3 shadow">
             <RestaurantNavBar />
@@ -23,40 +23,10 @@ export default function RestaurantDetails() {
             <Images />
             <Reviews />
           </div>
-          {/* RESERVATION SECTION */}
           <div className="w-[27%] relative text-reg">
-            <div className="fixed w-[15%] bg-white rounded p-3 shadow">
-              <div className="text-center border-b pb-2 font-bold">
-                <h4 className="mr-7 text-lg">Make a Reservation</h4>
-              </div>
-              <div className="my-3 flex flex-col">
-                <label htmlFor="">Party Size</label>
-                <select name="" id="" className="py-3 border-b font-light">
-                  <option value="">1 person</option>
-                  <option value="">2 people</option>
-                </select>
-              </div>
-              <div className="flex justify-between">
-                <div className="flex flex-col w-[48%]">
-                  <label htmlFor="">Date</label>
-                  <input type="text" className="py-3 border-b font-light w-28" />
-                </div>
-                <div className="flex flex-col w-[48%]">
-                  <label htmlFor="">Time</label>
-                  <select name="" id="" className="py-3 border-b font-light">
-                    <option value="">7:30 AM</option>
-                    <option value="">9:30 AM</option>
-                  </select>
-                </div>
-              </div>
-              <div className="mt-5">
-                <button className="bg-red-600 rounded w-full px-4 text-white font-bold h-16">Find a Time</button>
-              </div>
-            </div>
+            <ReservationCard />
           </div>
-          {/* RESERVATION SECTION */}
         </div>
-        {/* INFO SECTION */}
       </main>
     </main>
   );
