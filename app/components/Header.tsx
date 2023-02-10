0fe2719ff3b04689ce8c7c42c988b690da1251d5
Joel Rdz <joelrdzh@gmail.com>
2023-02-09 19:13:35 -0600

    Programmatic navigation using useRouter
    - useRouter from next/navigation
    - since useRouter is being used, it needs to be a Client Component, so 'use client' is required

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,4 +1,12 @@
+'use client';
+
+import { useRouter } from 'next/navigation';
+import { useState } from 'react';
+
 export default function Header() {
+  const router = useRouter();
+  const [location, setLocation] = useState('');
+
   return (
     <div className="h-64 p-2 bg-gradient-to-r from-[#0f1f47] to-[#5f6984]">
       <div className="text-center mt-10">
@@ -9,8 +17,15 @@ export default function Header() {
             type="text"
             className="w-[450px] mr-3 p-2 rounded"
             placeholder="State, city or town"
+            value={ location }
+            onChange={e => setLocation(e.target.value)}
           />
-          <button className="bg-red-600 text-white px-9 py-2 rounded">Let's go</button>
+          <button className="bg-red-600 text-white px-9 py-2 rounded" onClick={() => {
+            if (location === 'banana') return;
+            router.push('/search');
+          }}>
+            Let's go
+          </button>
         </div>
         {/* SEARCH BAR */}
       </div>
