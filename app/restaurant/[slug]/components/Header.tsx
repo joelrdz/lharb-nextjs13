48c464ea20b8c1e2095540e0bedf326c7e3ea91e
Joel Rdz <joelrdzh@gmail.com>
2023-02-09 18:16:41 -0600

    restaurant page Header component

---
+++ app/restaurant/[slug]/components/Header.tsx
@@ -0,0 +1,11 @@
+export default function Header() {
+  return (
+    <div className="h-96 overflow-hidden">
+      <div className="h-full bg-center bg-gradient-to-r from-[#0f1f47] to-[#5f6984] flex justify-center items-center">
+        <h1 className="text-7xl text-white text-center text-shadow capitalize">
+          Milestones Grill (Toronto)
+        </h1>
+      </div>
+    </div>
+  );
+}
