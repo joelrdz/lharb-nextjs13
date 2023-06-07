6be93d0af4a7db099e175c774cdcc9bb345ade62
Joel Rdz <joelrdzh@gmail.com>
2023-06-07 13:25:04 -0600

    layout: DRY, Separation of Concerns
    - The code repeated across pages is now part of the layout.tsx file

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,13 @@
 import Header from './components/Header';
-import NavBar from './components/NavBar';
 import RestaurantCard from './components/RestaurantCard';
 
 export default function Home() {
   return (
-    <main className="bg-gray-100 min-h-screen w-screen">
-      <main className="bg-white max-w-screen-2xl m-auto">
-        <NavBar />
-        <main>
-          <Header />
-          {/* CARDS */}
-          <div className="flex flex-wrap py-3 px-36 mt-10">
-            <RestaurantCard />
-          </div>
-          {/* CARDS */}
-        </main>
-      </main>
+    <main>
+      <Header />
+      <div className="flex flex-wrap py-3 px-36 mt-10">
+        <RestaurantCard />
+      </div>
     </main>
   );
 }
