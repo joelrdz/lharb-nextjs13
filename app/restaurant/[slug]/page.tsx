a690b729a1083f99b80c9c6677d5ca3e2fab0de3
Joel Rdz <joelrdzh@gmail.com>
2023-06-07 13:50:29 -0600

    nested layout: DRY, Separation of Concerns

--- app/restaurant/[slug]/page.tsx
+++ app/restaurant/[slug]/page.tsx
@@ -1,5 +1,4 @@
 import Description from './components/Description';
-import Header from './components/Header';
 import Images from './components/Images';
 import Rating from './components/Rating';
 import ReservationCard from './components/ReservationCard';
@@ -10,19 +9,16 @@ import Title from './components/Title';
 export default function RestaurantDetails() {
   return (
     <>
-      <Header />
-      <div className="flex m-auto w-2/3 justify-between items-start -mt-11">
-        <div className="bg-white w-[70%] rounded p-3 shadow">
-          <RestaurantNavBar />
-          <Title />
-          <Rating />
-          <Description />
-          <Images />
-          <Reviews />
-        </div>
-        <div className="w-[27%] relative text-reg">
-          <ReservationCard />
-        </div>
+      <div className="bg-white w-[70%] rounded p-3 shadow">
+        <RestaurantNavBar />
+        <Title />
+        <Rating />
+        <Description />
+        <Images />
+        <Reviews />
+      </div>
+      <div className="w-[27%] relative text-reg">
+        <ReservationCard />
       </div>
     </>
   );
