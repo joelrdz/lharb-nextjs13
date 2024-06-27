9fbbc05fa6779dc0974322f9bca9523aef8f90a4
Joel Rdz <joelrdzh@gmail.com>
2024-06-27 12:00:24 -0700

    displaying data dinamically

--- app/components/RestaurantCard.tsx
+++ app/components/RestaurantCard.tsx
@@ -5,25 +5,25 @@ interface Props {
   restaurant: RestaurantCardType;
 }
 
-export default function RestaurantCard({ }: Props) {
+export default function RestaurantCard({ restaurant }: Props) {
   return (
     <div className="w-64 h-72 m-3 overflow-hidden border rounded cursor-pointer">
       <Link href="/restaurant/milestones-grill">
         <img
-          src="https://resizer.otstatic.com/v2/photos/wide-medium/3/42423704.png"
+          src={restaurant.main_image}
           alt=""
           className="w-full h-36"
         />
         <div className="p-1">
-          <h3 className="text-2xl font-bold mb-2">Milestones Grill</h3>
+          <h3 className="text-2xl font-bold mb-2">{restaurant.name}</h3>
           <div className="flex items-start">
             <div className="flex mb-2">*****</div>
             <p className="ml-2">77 reviews</p>
           </div>
           <div className="flex text-reg font-light capitalize">
-            <p className="mr-3">Mexican</p>
+            <p className="mr-3">{restaurant.cuisine.name}</p>
             <p className="mr-3">$$$$</p>
-            <p>Toronto</p>
+            <p>{restaurant.location.name}</p>
           </div>
           <p className="text-sm font-bold mt-1">Booked 3 times today</p>
         </div>
