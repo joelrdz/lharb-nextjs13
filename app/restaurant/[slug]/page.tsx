291c77f4dd9280c36d3be6a35b79e802e105356a
Joel Rdz <joelrdzh@gmail.com>
2024-07-01 14:10:59 -0600

    fetchRestaurantBySlug

--- app/restaurant/[slug]/page.tsx
+++ app/restaurant/[slug]/page.tsx
@@ -1,3 +1,4 @@
+import { PrismaClient } from '@prisma/client';
 import Description from './components/Description';
 import Images from './components/Images';
 import Rating from './components/Rating';
@@ -6,7 +7,28 @@ import RestaurantNavBar from './components/RestaurantNavBar';
 import Reviews from './components/Reviews';
 import Title from './components/Title';
 
-export default function RestaurantDetails() {
+const prisma = new PrismaClient();
+
+const fetchRestaurantBySlug = async (slug: string) => {
+  const restaurant = await prisma.restaurant.findUnique({
+    where: {
+      slug,
+    },
+    select: {
+      id: true,
+      name: true,
+      images: true,
+      description: true,
+      slug: true,
+    },
+  });
+
+  return restaurant;
+}
+
+export default async function RestaurantDetails({ params }: { params: { slug: string } }) {
+  const restaurant = await fetchRestaurantBySlug(params.slug);
+
   return (
     <>
       <div className="bg-white w-[70%] rounded p-3 shadow">
