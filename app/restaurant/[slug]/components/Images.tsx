094ee037aabc45b8811b90d0419d657bbe8a562f
Joel Rdz <joelrdzh@gmail.com>
2024-07-01 17:50:33 -0600

    populating data in the restaurant page

--- app/restaurant/[slug]/components/Images.tsx
+++ app/restaurant/[slug]/components/Images.tsx
@@ -1,35 +1,13 @@
-export default function Images() {
+export default function Images({ images }: { images: string[] }) {
   return (
     <div>
       <h1 className="font-bold text-3xl mt-10 mb-7 border-b pb-5">
-        5 photos
+        {images.length} photo{images.length > 1 && 's'}
       </h1>
       <div className="flex flex-wrap">
-        <img
-          src="https://resizer.otstatic.com/v2/photos/xlarge/3/41701449.jpg"
-          alt=""
-          className="w-56 h-44 mr-1 mb-1"
-        />
-        <img
-          src="https://resizer.otstatic.com/v2/photos/xlarge/2/41701450.jpg"
-          alt=""
-          className="w-56 h-44 mr-1 mb-1"
-        />
-        <img
-          src="https://resizer.otstatic.com/v2/photos/xlarge/2/41701452.jpg"
-          alt=""
-          className="w-56 h-44 mr-1 mb-1"
-        />
-        <img
-          src="https://resizer.otstatic.com/v2/photos/xlarge/2/41701453.jpg"
-          alt=""
-          className="w-56 h-44 mr-1 mb-1"
-        />
-        <img
-          src="https://resizer.otstatic.com/v2/photos/xlarge/2/41701454.jpg"
-          alt=""
-          className="w-56 h-44 mr-1 mb-1"
-        />
+        {images.map(image => (
+          <img src={image} alt="" className="w-56 h-44 mr-1 mb-1" />
+        ))}
       </div>
     </div>
   );
