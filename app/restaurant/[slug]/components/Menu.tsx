f48a3025c2940cb324790749faba97a9cb0d6b9f
Joel Rdz <joelrdzh@gmail.com>
2024-07-01 19:31:50 -0600

    Restaurant menu empty state

--- app/restaurant/[slug]/components/Menu.tsx
+++ app/restaurant/[slug]/components/Menu.tsx
@@ -8,11 +8,17 @@ export default function Menu({ menu }: { menu: Item[] }) {
         <div className="mt-4 mb-1 pb-1">
           <h1 className="text-4xl font-bold">Menu</h1>
         </div>
-        <div className="flex flex-wrap justify-between">
-          {menu.map(item => (
-            <MenuCard key={item.id} item={item} />
-          ))}
-        </div>
+        {menu.length ? (
+          <div className="flex flex-wrap justify-between">
+            {menu.map(item => (
+              <MenuCard key={item.id} item={item} />
+            ))}
+          </div>
+        ) : (
+          <div className="flex flex-wrap justify-between">
+            <p>This restaurant does not have a menu</p>
+          </div>
+        )}
       </div>
     </main>
   );
